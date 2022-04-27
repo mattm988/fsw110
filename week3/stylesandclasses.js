0e60cb0a22fe1c4bef1a98092b20ce4cb73e72b5
mattm988 <98669734+mattm988@users.noreply.github.com>
2022-04-27 18:32:10 -0400

    Create stylesandclasses.js

---
+++ week3/stylesandclasses.js
@@ -0,0 +1,11 @@
+for(var i=0; i<5; i++){
+let h2 = document.createElement('h2')
+h2.textContent = "Hello"
+document.body.appendChild(h2)
+h2.style.fontSize = '20px'
+h2.style.fontWeight = 'lighter'
+h2.style.fontFamily = 'sans-serif'
+h2.style.color = 'cornflowerblue'
+h2.className = "border"
+console.log(h2)
+}
